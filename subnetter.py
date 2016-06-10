51b4a83f79a6322259da4b9d0537a66a1261b6d6
Peter-J-W <wagnerpj2011@gmail.com>
2016-06-09 20:24:35 -0400

    Updated to Python 3
    Ported to Python 3, added host ranges, classful messaging

--- subnetter.py
+++ subnetter.py
@@ -12,7 +12,7 @@ def calc_next_bound(cdr):
 		return 0
 
 def calc_block(cdr, next_bound):
-	return math.pow(2, (next_bound - cdr))
+	return 2**(next_bound - cdr)
 
 def cidr_valid(cdr):
 	if cdr >= 1 and cdr <= 31:
@@ -20,13 +20,23 @@ def cidr_valid(cdr):
 	else:
 		return False
 
+def classful_message(cdr):
+	print("\nThe CIDR notation you entered represents a classful network.")
+	if cdr == 8:
+		print("Class A addresses allow 126 networks with 16,777,214 usable hosts per network.")
+	elif cdr == 16:
+		print("Class B addresses allow 16,382 networks with 65,534 usable hosts per network.")
+	elif cdr == 24:
+		print("Class C addresses allow 2,097,150 networks with 254 usable hosts per network.")
+
 def is_classful(cdr):
 	if cdr == 8 or cdr == 16 or cdr == 24:
 		return True
 	else:
 		return False
+	
 
-#Takes the IP Address as a raw_input string. Using the dot separators as positions,
+#Takes the IP Address as an input string. Using the dot separators as positions,
 # slices the string and populates a dictionary, which is then returned.
 def octet_splitter(ipAdd):
 	first_dot = ipAdd.index('.')
@@ -57,25 +67,40 @@ def subnetA(ipDict, cdr):
 	next_boundary = calc_next_bound(cdr)
 	block_size = calc_block(cdr, next_boundary)
 
-	for sub in range(0, 255, int(block_size)):
-		print str(ipDict['FirstOctet']) + '.' + str(sub) + '.0' + '.0'
+	count = 1
+	for sub in range(0, 255, block_size):
+		print('Subnet Address #' + str(count) + ' = ' , end = '')
+		print(str(ipDict['FirstOctet']) + '.' + str(sub) + '.0' + '.0', end = '')
+		print(' ------- Host Range = ' + str(ipDict['FirstOctet']) + '.' + str(sub) + '.0' + '.1', end = '')
+		print(' - ' + str(ipDict['FirstOctet']) + '.' + str((sub + block_size) - 1) + '.255' + '.254')
 		sub = sub + block_size
+		count += 1
 
 def subnetB(ipDict, cdr):
 	next_boundary = calc_next_bound(cdr)
 	block_size = calc_block(cdr, next_boundary)
 
-	for sub in range(0, 255, int(block_size)):
-		print str(ipDict['FirstOctet']) + '.' + str(ipDict['SecondOctet']) + '.' + str(sub) + '.0'
+	count = 1
+	for sub in range(0, 255, block_size):
+		print('Subnet Address #' + str(count) + ' = ' , end ='')
+		print(str(ipDict['FirstOctet']) + '.' + str(ipDict['SecondOctet']) + '.' + str(sub) + '.0', end = '')
+		print(' ------- Host Range = ' + str(ipDict['FirstOctet']) + '.' + str(ipDict['SecondOctet']) + '.' + str(sub) + '.1', end = '')
+		print(' - ' + str(ipDict['FirstOctet']) + '.' + str(ipDict['SecondOctet']) + '.' + str((sub + block_size) - 1) + '.254')
 		sub = sub + block_size
+		count += 1
 
 def subnetC(ipDict, cdr):
 	next_boundary = calc_next_bound(cdr)
 	block_size = calc_block(cdr, next_boundary)
 
-	for sub in range(0, 255, int(block_size)):
-		print str(ipDict['FirstOctet']) + '.' + str(ipDict['SecondOctet']) + '.' + str(ipDict['ThirdOctet']) + '.' + str(sub)
+	count = 1
+	for sub in range(0, 255, block_size):
+		print('Subnet Address #' + str(count) + ' = ' , end ='')
+		print(str(ipDict['FirstOctet']) + '.' + str(ipDict['SecondOctet']) + '.' + str(ipDict['ThirdOctet']) + '.' + str(sub), end ='')
+		print(' ------- Host Range = ' + str(ipDict['FirstOctet']) + '.' + str(ipDict['SecondOctet']) + '.' + str(ipDict['ThirdOctet']) + '.' + str(sub + 1), end = '')
+		print(' - ' + str(ipDict['FirstOctet']) + '.' + str(ipDict['SecondOctet']) + '.' + str(ipDict['ThirdOctet']) + '.' + str((sub + block_size) - 2))
 		sub = sub + block_size
+		count += 1
 
 def display_results(cdr, ipDict):
 	boundary = calc_next_bound(cdr)
@@ -87,14 +112,14 @@ def display_results(cdr, ipDict):
 	elif boundary == 32:
 		subnetC(ipDict, cdr)
 	else:
-		print "Error Subnetting"
+		pass
 
 #Main Program Loop
 running = True
 while running == True:
 
 	#Allow users to quit or continue subnetting
-	user_query = raw_input("Would you like to subnet (y/n)? ")
+	user_query = input("\nWould you like to subnet (y/n)? (Entering \'n\' exits.)")
 	if user_query == 'n':
 		running = False
 		break
@@ -105,25 +130,31 @@ while running == True:
 	ipValid = False
 	while ipValid == False:
 		try:
-			ipAdd = raw_input("Enter the I.P. Address (X.X.X.X): ")
+			ipAdd = input("Enter the I.P. Address (X.X.X.X): ")
 			ipAddDict = octet_splitter(ipAdd)
 			ipValid = ipAdd_valid(ipAddDict)
 			if ipValid == False:
-				print "\n**Enter valid octets (0 - 255)**\n"
+				print("\n**Enter valid octets (0 - 255)**\n")
 		except:
-			print "\n**Enter a valid I.P. Address in the (X.X.X.X) format**\n"
+			print("\n**Enter a valid I.P. Address in the (X.X.X.X) format**\n")
 			continue
 
 	#Get CIDR notation number and validate input
 	cidrValid = False
 	while cidrValid == False:
 		try:
-			cidr = int(raw_input("Enter the CIDR notation number: /"))
+			cidr = int(input("Enter the CIDR notation number: /"))
 			cidrValid = cidr_valid(cidr)
+			classful = is_classful(cidr)
+			if classful == True:
+				classful_message(cidr)
+				break
+			else:
+				continue
 			if cidrValid == False:
-				print "\n**Enter a valid CIDR notation number (Integer 1 - 31)**\n"
+				print("\n**Enter a valid CIDR notation number (Integer 1 - 31)**\n")
 		except:
-			print "\n**Enter a valid CIDR notation number (Integer 1 - 31)**\n"
+			print("\n**Enter a valid CIDR notation number (Integer 1 - 31)**\n")
 			continue
 
 	#Display Subnet Addresses
